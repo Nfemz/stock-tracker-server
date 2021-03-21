f61a414a68544dfa2bf86fb542357cfdd522aed4
Nicholas Femia <femianicholas@gmail.com>
2021-03-21 14:22:17 -0400

    set up webhook <-> http polling proxy for client

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,8 @@ const PORT = 9000;
 
 app.ws("/subscribe/reddit", (ws: any, req: any) => {
   const { url } = req.body;
+  console.warn(url);
   subscribeSubredditPost(url, ws);
 });
+
+app.listen(PORT, () => console.info(`Server listening on Port:${PORT}`));
